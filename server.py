0299da05253b04c2acb5c276a273ab9fc6493527
Manish Kumar <1709manishbkp@gmail.com>
2025-09-28 12:19:57 +0530

    Trigger new diploy

--- server.py
+++ server.py
@@ -70,3 +70,4 @@ def chat():
 if __name__ == '__main__':
     print("Starting Flask server... Your portfolio will be live at http://127.0.0.1:5000")
     app.run(debug=True, host='0.0.0.0', port=5000)
+
